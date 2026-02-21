faf252c004172093fbd1bf2406669f5a7fb06615
devilsuse <nanosoft.indian@gmail.com>
2026-02-21 18:57:19 +0530

    s

---
+++ src/com/nano/youtube/CastExceptionNotDetectedAtCompiletime.java
@@ -0,0 +1,20 @@
+package com.nano.youtube;
+
+public class CastExceptionNotDetectedAtCompiletime {
+    public static void main(String[] args) {
+        Rectangle r = new ABC();//new Rectangle();
+        Polygon p = (Polygon) r;
+    }
+}
+
+interface Polygon{}
+final class Rectangle {//implements Polygon{
+    public Rectangle(){
+        System.out.println("works !");
+    }
+}
+
+//class ABC extends Rectangle implements Polygon{
+//}
+
+
