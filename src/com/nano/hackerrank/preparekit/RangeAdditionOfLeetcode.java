754601b32db816a38c4010bf975ecd8329ec4758
devilsuse <nanosoft.indian@gmail.com>
2026-02-26 23:20:50 +0530

    vvvvvvvvvvvvvvvv

--- src/com/nano/hackerrank/preparekit/RangeAdditionOfLeetcode.java
+++ src/com/nano/hackerrank/preparekit/RangeAdditionOfLeetcode.java
@@ -114,7 +114,7 @@ public class RangeAdditionOfLeetcode {
     */
 }
 
-public class Solution {
+class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
